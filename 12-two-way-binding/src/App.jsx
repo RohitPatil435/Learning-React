d7cd27fa1c25d71281f4df7366035e9f30006e3a
RohitPatil435 <rohitgpatil1221@gmail.com>
2025-11-01 17:29:28 +0530

    Day-7

---
+++ 12-two-way-binding/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+  const [title, setTitle] = useState('')
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("form submited by" , title);
+    setTitle('')
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input type="text" placeholder="Enter ur name" 
+        value={title}
+        onChange={(e)=>{
+          setTitle(e.target.value);
+        }}/>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
